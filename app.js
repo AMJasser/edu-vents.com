7bc838f0390a571950234f791d267f8ee25cf4a8
Abdullah AlJasser <amj.jasser@gmail.com>
2021-02-02 13:12:46 +0300

    initial

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const methodOverride = require("method-override");
+const path = require("path");
+const errorHandler = require("./middleware/error");
+const connectDb = require("./config/db");
+
+// Load ENV vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to DB
+connectDb();
+
+// Route files
+
+const app = express();
+
+// Set view engine
+app.set("view engine", "ejs");
+
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Method Override
+app.use(methodOverride("_method"));
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Mount routers
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`.bold.cyan));
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
